4d16adb63a601719582425182974213b5184a154
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-07-27 12:18:24 -0700

    route delete

--- routes/companies.js
+++ routes/companies.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const slugify = require("slugify");
 const router = express.Router();
 const ExpressError = require("../expressError");
 const db = require("../db");
@@ -36,7 +37,15 @@ router.get("/:code", async function (req, res, next) {
 // POST Route
 router.post("/", async (req, res, next) => {
   try {
-    const { code, name, description } = req.body;
+    const { name, description } = req.body;
+    //slugify to standardize name into lowercase & hypen code
+    const code = slugify(name, { lower: true });
+
+    //throw error if not complete field
+    if (!code || !name || !description) {
+      throw new ExpressError("Missing required field", 400);
+    }
+
     const results = await db.query(
       `INSERT INTO companies (code, name, description) 
             VALUES ($1, $2, $3) 
@@ -50,6 +59,39 @@ router.post("/", async (req, res, next) => {
 });
 
 // PUT Route
+router.put("/:code", async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const results = await db.query(
+      `INSERT INTO companies (code, name, description) 
+              VALUES ($1, $2, $3) 
+              RETURNING code, name, description`,
+      [code, name, description]
+    );
+    return res.status(201).json({ company: results.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
 // DELETE Route
+
+router.delete("/:code", async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const result = await db.query(
+      `DELETE FROM companies 
+            WHERE code=$1 
+            RETURNING code`,
+      [code]
+    );
+    if (result.rowCount === 0) {
+      throw new ExpressError(`Company Code: ${code} not found`, 404);
+    }
+    return res.send({ msg: `Deleted comapny: ${code}` });
+  } catch (e) {
+    return next(e);
+  }
+});
 
 module.exports = router;
