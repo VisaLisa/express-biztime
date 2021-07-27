31219163814788989ad371f1a0deca57fe3a93c6
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-07-27 12:01:09 -0700

    post route

--- routes/companies.js
+++ routes/companies.js
@@ -34,6 +34,21 @@ router.get("/:code", async function (req, res, next) {
   }
 });
 // POST Route
+router.post("/", async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const results = await db.query(
+      `INSERT INTO companies (code, name, description) 
+            VALUES ($1, $2, $3) 
+            RETURNING code, name, description`,
+      [code, name, description]
+    );
+    return res.status(201).json({ company: results.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
 // PUT Route
 // DELETE Route
 
